35447e8cc5241aeb5c54440b3c40637b96c61a10
Maria Folvarska <mfolvarska12@gmail.com>
2017-02-02 23:23:20 -0600

    eml

--- contact.php
+++ contact.php
@@ -51,21 +51,20 @@ if (isset($_POST['email'])){
     function clean_string($string){
       $bad = array("content-type", "bcc:", "to:", "cc:", "href");
       return str_replace($bad, "", $string);
-
+      }
     $email_message .= "Name:" . clean_string($name) . "\n";
     $email_message .= "Email:" . clean_string($email) . "\n";
     $email_message .= "Message:" . clean_string($message) . "\n";
-    }
+
     //create email headers
     $headers = 'From: ' . $email_From . "\r\n". 'Reply-To:' . $email
     "\r\n" .
     'X-Mailer: PHP/' . phpversion();
     @mail($email_to, $email_subject, $email_message, $headers);
-
  ?>
-
  <!--success message-->
  Thank you for contacting me. I will be in touch with you shortly. <br/>
+ Please click <a href="index.html">here</a> to go back to the home page.
 <?php
  }
 ?>
