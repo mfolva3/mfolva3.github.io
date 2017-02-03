bc555a7567f4e1d654f0be4b520df13c0e822697
Maria Folvarska <mfolvarska12@gmail.com>
2017-02-02 23:10:55 -0600

    email contact

--- contact.php
+++ contact.php
@@ -32,9 +32,40 @@ if (isset($_POST['email'])){
     $email_exp = '/^[A-Za-z0-9._%-]+[[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
     if(!preg_match($email_exp, $email)){
       $error_message .= 'The email address you entered does not appear to
-       be valid.';
+       be valid.<br/>';
+        }
+    $string_exp = "/^[A-Za-z.'-]+$/";
+    if (!preg_math($string_exp, $name)){
+      $error_message .= 'The name you entered does not appear to be valid.
+      <br/>';
     }
+    if(strlen($message) <2 ){
+      $error_message .= 'The message you entered does not appear to be valid.
+      <br/>';
+      }
+    if(strlen($error_message) > 0 ){
+      died($error_message)
+      }
+      $email_message = "Form details below.\n\n";
 
-}
+    function clean_string($string){
+      $bad = array("content-type", "bcc:", "to:", "cc:", "href");
+      return str_replace($bad, "", $string);
+
+    $email_message .= "Name:" . clean_string($name) . "\n";
+    $email_message .= "Email:" . clean_string($email) . "\n";
+    $email_message .= "Message:" . clean_string($message) . "\n";
+    }
+    //create email headers
+    $headers = 'From: ' .$email_From . "\r\n". 'Reply-To:' . $email
+    "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+    @mail($email_to, $email_subject, $email_message, $headers);
 
  ?>
+
+ <!--success message-->
+ Thank you for contacting me. I will be in touch with you shortly. <br/>
+<?php
+ }
+?>
